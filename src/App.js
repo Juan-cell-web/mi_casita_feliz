1f9c75c26f4cbbb3d3f848101b29b44a7f348f7d
juan-cell-web <correodejuanmiguel@gmail.com>
2024-11-12 20:35:16 -0500

    mi buen teclado

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://github.com/Juan-cell-web"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Juan Peñuela
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
